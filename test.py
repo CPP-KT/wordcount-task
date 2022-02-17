a3d0ee4aace01d2bfe422af9882976b903da0d16
Nursan Valeyev <tindarid@gmail.com>
2022-02-17 20:31:16 +0300

    Big tests

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import random
 import string
 from subprocess import Popen, PIPE
 
-def rand_string(size):
+def rand_string(size, sep = ''):
     good_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    return ''.join(random.choices(population=good_chars, k=size)).encode('utf-8')
+    return sep.join(random.choices(population=good_chars, k=size)).encode('utf-8')
 
 
 def rand_spaces(size):
@@ -16,7 +16,16 @@ def rand_spaces(size):
 def gen_input(stream, words_cnt, front_spaces, back_spaces):
     if front_spaces:
         stream.write(rand_spaces(random.randint(1, 10)))
-    if words_cnt >= 1:
+    if words_cnt >= int(1e5):
+        chunk_size = int(1e4)
+        (chunks, delta) = divmod(words_cnt, chunk_size)
+        big_chunk = rand_spaces(1) + rand_string(chunk_size, ' ')
+        for _ in range(chunks):
+            stream.write(big_chunk)
+        for _ in range(delta):
+            stream.write(rand_spaces(random.randint(1, 50)))
+            stream.write(rand_string(random.randint(1, 50)))
+    elif words_cnt >= 1:
         stream.write(rand_string(random.randint(1, 50)))
         for _ in range(words_cnt - 1):
             stream.write(rand_spaces(random.randint(1, 50)))
@@ -26,8 +35,7 @@ def gen_input(stream, words_cnt, front_spaces, back_spaces):
 
 
 def test(words_cnt, front_spaces, back_spaces):
-    with Popen('./wordcount', shell=True, bufsize=-1,
-               stdin=PIPE, stdout=PIPE, close_fds=True) as process:
+    with Popen('./wordcount', shell=True, stdin=PIPE, stdout=PIPE, close_fds=True) as process:
         gen_input(process.stdin, words_cnt, front_spaces, back_spaces)
 
         process.stdin.close()
@@ -53,3 +61,6 @@ test_all_spaces(0)
 for _ in range(100):
     cnt = random.randint(100, 5000)
     test_all_spaces(cnt)
+for huge in map(int, [1e6, 1e7, 1e8, 1e9]):
+    test_all_spaces(huge + 13)
+test_all_spaces(2 ** 32 + 42)
